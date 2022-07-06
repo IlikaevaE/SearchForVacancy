3540a233b08843091acd11925df03bde7aeabf9c
IlikaevaE <gud_135win@mail.ru>
2022-07-06 15:51:58 +0300

    test steps updated

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -15,100 +15,112 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class GeneratedTests extends TestBase {
+    private final String BASEURL = "https://voronezh.hh.ru/";
+    private final String SECOND_URL = "https://voronezh.hh.ru/search/vacancy?text=selenide&from=suggest_post&fromSearchLine=true&area=26";
+    private final String BASE_TEXT = "Работа найдется для каждого";
+    private final String SEARCH_VALUE = "Selenide";
+    private final String VACANCY_NAME = "QA Automation Engineer (Java) / инженер по автоматизированному тестированию";
+    private final String ANSWER_VACANCY = "Откликнуться";
+    private final String PARAMETER_1 = "Можно работать из дома";
+    private final String PARAMETER_2 = "Показать контакты";
+    private final String CITY_NAME = "Воронеж";
+    private final String COMPANY_NAME = "ITooLabs";
+
     @Test
-    @DisplayName("Opening the start page")
+    @DisplayName("Проверяем работу сайта вакансий")
     void openVacancyPage() {
 
-        step("Open the vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/");
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(BASEURL);
         });
 
-        step("Checking we opened the right vacancy page", () -> {
-            $(".supernova-dashboard-search").shouldHave(Condition.text("Работа найдется для каждого"));
+        step("Проверяем, что нужная страница открылась", () -> {
+            $(".supernova-dashboard-search").shouldHave(Condition.text(BASE_TEXT));
 
         });
     }
 
     @Test
-    @DisplayName("Searching for the defined vacancy")
+    @DisplayName("Ищем вакансию по скилу Selenide")
     void searchForVacancy() {
-        step("Open the vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/");
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(BASEURL);
         });
-        step("Click on the search input field", () -> {
+        step("Ставим курсор в поле для поиска", () -> {
             $("[id=a11y-search-input]").click();
         });
-        step("Type the word \"Selenide\"", () -> {
-            $("[id=a11y-search-input]").sendKeys("Selenide");
+        step("Вводим значение \"Selenide\"", () -> {
+            $("[id=a11y-search-input]").sendKeys(SEARCH_VALUE);
         });
 
-        step("Submit the search", () -> {
+        step("Подтверждаем поиск", () -> {
             $(".supernova-search-group__input").submit();
         });
 
-        step("Checking the defined vacancy exists", () -> {
-            $(".vacancy-serp-content").shouldHave(Condition.text("QA Automation Engineer (Java) / инженер по автоматизированному тестированию"));
+        step("Проверяем, что вакансия с заданными параметрами существует", () -> {
+            $(".vacancy-serp-content").shouldHave(Condition.text(VACANCY_NAME))
+                    .shouldHave(Condition.text(ANSWER_VACANCY))
+                    .shouldHave(Condition.text(PARAMETER_1))
+                    .shouldHave(Condition.text(PARAMETER_2));
         });
     }
 
     @Test
+    @DisplayName("Проверяем местоположение вакансии" )
     void checkCity() {
-        step("Open the vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/");
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(BASEURL);
         });
-        step("Click on the search input field", () -> {
+        step("Ищем вакансию с ключом \"Selenide\"", () -> {
             $("[id=a11y-search-input]").click();
-        });
-        step("Type the word \"Selenide\"", () -> {
-            $("[id=a11y-search-input]").sendKeys("Selenide");
-        });
-
-        step("Submit the search", () -> {
+            $("[id=a11y-search-input]").sendKeys(SEARCH_VALUE);
             $(".supernova-search-group__input").submit();
         });
-        step("Check the location of vacancy", () -> {
-            $(".vacancy-serp-item-body").shouldHave(Condition.text("Воронеж"));
+        step("Проверяем, что вакансия находится в нужном городе", () -> {
+            $(".vacancy-serp-item-body").shouldHave(Condition.text(CITY_NAME));
         });
     }
 
     @Test
+    @DisplayName("Проверяем принадлежность вакансии компании ITooLabs")
     void checkCompany() {
-        step("Open vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/search/vacancy?text=selenide&from=suggest_post&fromSearchLine=true&area=26");
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(SECOND_URL);
         });
-        step("Checking the company name", () -> {
-            $(".vacancy-serp-item-body").shouldBe(Condition.text("ITooLabs"));
-        });
-    }
-    @Test
-    void answerVacancyButton() {
-        step("Open vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/search/vacancy?text=selenide&from=suggest_post&fromSearchLine=true&area=26");
-        });
-        step("Checking the visibility of button", () -> {
-            $(Selectors.byText("Откликнуться")).shouldBe(Condition.visible);
+        step("Проверяем, что вакансия принадлежить заданной компании", () -> {
+            $(".vacancy-serp-item-body").shouldBe(Condition.text(COMPANY_NAME));
         });
     }
 
     @Test
-   @Disabled
+    @DisplayName("Проверяем видимость кнопки 'Откликнуться'")
+    void answerVacancyButton() {
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(SECOND_URL);
+        });
+        step("Проверяем, что кнопка 'Откликнуться' видима", () -> {
+            $(Selectors.byText(ANSWER_VACANCY)).shouldBe(Condition.visible);
+        });
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Тест в разработке")
     void answerVacancy() {
-        step("Open vacancy page hh.ru", () -> {
-            open("https://voronezh.hh.ru/search/vacancy?text=selenide&from=suggest_post&fromSearchLine=true&area=26");
+        step("Открываем сайт вакансий hh.ru", () -> {
+            open(SECOND_URL);
         });
         $(".bloko-button").find(withText("Откликнуться")).click();
     }
 
     @Test
-    @DisplayName("Page console log should not have errors")
+    @DisplayName("В Concole Log страницы не должно быть ошибок")
     void consoleShouldNotHaveErrorsTest() {
-        step("Open URL for vacancy", () ->
-                open("https://voronezh.hh.ru/"));
-
-        step("Console logs should not contain text 'SEVERE'", () -> {
+        step("Открываем сайт вакансий hh.ru", () ->
+                open(BASEURL));
+        step("В консоле ошибок не должно содержаться текста 'SEVERE'", () -> {
             String consoleLogs = DriverUtils.getConsoleLogs();
             String errorText = "SEVERE";
-
             assertThat(consoleLogs).doesNotContain(errorText);
         });
     }
